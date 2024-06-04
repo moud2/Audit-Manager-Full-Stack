2ef874770f96746416ca03c21da73e7423a81f76
Janis Kupfer <janis.kupfer@stud.htwk-leipzig.de>
2024-06-04 18:29:47 +0200

    fix: fix 404 error for GET request

--- src/main/java/com/insight/backend/GetCategoriesController.java
+++ src/main/java/com/insight/backend/GetCategoriesController.java
@@ -5,11 +5,12 @@ import org.springframework.http.ResponseEntity;
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class GetCategoriesController {
 
-    @GetMapping("/api/hw")
-    public ResponseEntity<List<Category>> getCategory() {
-        List<Category> response = new ArrayList<Category>();
+    @GetMapping("api/hw")
+    public ResponseEntity<Object> getCategory() {
+        List<Category> response = new ArrayList<>();
         response.add(new Category(0, "a"));
         response.add(new Category(1, "b"));
         response.add(new Category(2, "c"));
