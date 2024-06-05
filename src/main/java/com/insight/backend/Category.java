3f85267e598fa67d91e599f375ce18d537c66453
Janis Kupfer <janis.kupfer@stud.htwk-leipzig.de>
2024-06-05 10:36:40 +0200

    fix: return values with GSON

--- src/main/java/com/insight/backend/Category.java
+++ src/main/java/com/insight/backend/Category.java
@@ -1,11 +1,14 @@
 package com.insight.backend;
 
+/*placeholder class for Categories */
 public class Category {
         private int id;
         private String name;
     
-        public Category(int id, String name){
+        public Category(int id, String name) {
             this.id = id;
             this.name = name;
         }
+
+        
     }
--- src/main/java/com/insight/backend/GetCategoriesController.java
+++ src/main/java/com/insight/backend/GetCategoriesController.java
@@ -1,6 +1,8 @@
 package com.insight.backend;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,12 +10,15 @@ import java.util.List;
 @RestController
 public class GetCategoriesController {
 
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    /*GET request fpr categories with placeholder data */
     @GetMapping("api/hw")
     public ResponseEntity<Object> getCategory() {
         List<Category> response = new ArrayList<>();
         response.add(new Category(0, "a"));
         response.add(new Category(1, "b"));
         response.add(new Category(2, "c"));
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(gson.toJson(response));
     }
 }
