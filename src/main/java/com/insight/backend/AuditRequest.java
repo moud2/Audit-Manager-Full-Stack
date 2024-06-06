54d862460c495957938ee48e7bf05b3a5d5546e4
Fabian Müller <fabian.mueller@stud.htwk-leipzig.de>
2024-06-06 16:39:46 +0200

    feat: api new audit build

---
+++ src/main/java/com/insight/backend/AuditRequest.java
@@ -0,0 +1,25 @@
+package com.insight.backend;
+
+import java.util.List;
+
+public class AuditRequest {
+    private String name;
+    private List<Integer> categories;
+
+    // Getter und Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Integer> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Integer> categories) {
+        this.categories = categories;
+    }
+}
