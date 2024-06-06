1adee4ac71e2f0217e48df9d1dda89e1a86e5a45
Thomas Mugler <thomas.mugler@stud.htwk-leipzig.de>
2024-06-06 17:04:50 +0200

    style: tidy up model and controller
    tidy up HelloWorldController and GetRatingController

--- src/main/java/com/insight/backend/controller/GetRatingController.java
+++ src/main/java/com/insight/backend/controller/GetRatingController.java
@@ -1,7 +1,6 @@
 package com.insight.backend.controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.http.HttpStatus;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.util.ArrayList;
@@ -14,15 +13,15 @@ import com.insight.backend.model.RatingList;
 @RestController
 public class GetRatingController {
 
-    @GetMapping("/api/v1/audits/{ratingId}/ratings")
-    public ResponseEntity<String> get(@PathVariable("ratingId") Integer ratingId) {
+    @GetMapping("/api/v1/audits/{auditId}/ratings")
+    public ResponseEntity<String> get(@PathVariable("auditId") Integer auditId) {
         final Gson gson = new GsonBuilder().setPrettyPrinting().create();
         
         //Rating 1 und 2 zum Abschicken erstellen
         Rating rating1 = new Rating(1, "Bob", "Kommentar1", 0, true);
         Rating rating2 = new Rating(2, "Ben", "Kommentar2", 1, false);
-        Rating rating3 = new Rating(2, "Boris", "Kommentar3", 2, false);
-        Rating rating4 = new Rating(2, "Berthold", "Kommentar4", 3, false);
+        Rating rating3 = new Rating(3, "Boris", "Kommentar3", 2, false);
+        Rating rating4 = new Rating(4, "Berthold", "Kommentar4", 3, false);
 
         //Liste aus Ratings machen
         List<Rating> ratings1 = new ArrayList<Rating>();
@@ -34,11 +33,11 @@ public class GetRatingController {
         ratings2.add(rating4);
         RatingList ratingList2 = new RatingList(ratings2);
 
-        //Ratings in per ID abrufbare Map
+        //Ratings in per ID abrufbarer Map -> AuditID-Simulation
         HashMap<Integer, RatingList> ratingsAssigned = new HashMap<Integer, RatingList>();
         ratingsAssigned.put(1, ratingList1);
         ratingsAssigned.put(2, ratingList2);
-        RatingList ratingListSend = ratingsAssigned.get(ratingId);
+        RatingList ratingListSend = ratingsAssigned.get(auditId);
         
         return ResponseEntity.ok(gson.toJson(ratingListSend));
     }
