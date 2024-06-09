a8fe2223c8041f4406f8cb461f2f4e8c59b91431
Janis Kupfer <janis.kupfer@stud.htwk-leipzig.de>
2024-06-09 19:29:22 +0200

    fix: add getters and setters for Category class, write .gitignore file

--- src/main/java/com/insight/backend/model/Category.java
+++ src/main/java/com/insight/backend/model/Category.java
@@ -1,14 +1,27 @@
 package com.insight.backend.model;
 
-/*placeholder class for Categories */
 public class Category {
-        private int id;
-        private String name;
-    
-        public Category(int id, String name) {
-            this.id = id;
-            this.name = name;
-        }
+    private int id;
+    private String name;
 
-        
+    public Category(int id, String name) {
+        setCategoryId(id);
+        setCategoryName(name);
     }
+
+    public void setCategoryId(int id) {
+        this.id = id;
+    }
+
+    public int getCategoryId() {
+        return this.id;
+    }
+
+    public void setCategoryName(String name) {
+        this.name = name;
+    }
+
+    public String getCategoryName() {
+        return this.name;
+    }
+}
