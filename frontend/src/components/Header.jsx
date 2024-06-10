dc3e67ed1b5a502c8fc7ba5adfb99da253d1bfff
Anna Liepelt <anna.liepelt@stud.htwk-leipzig.de>
2024-06-10 18:17:32 +0200

    docs: add documentation

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
         <link href="./output.css" rel="stylesheet"></link>
         <header className="fixed top-1 left-1 w-full bg-white shadow-md flex items-center justify-between p-4 z-10">
             <div className="flex items-center">
-                <img src={logo} alt="Softline Logo" className="h-15 w-20 mr-5" />
+                <img id="softline-logo" src={logo} alt="Softline Logo" className="h-15 w-20 mr-5" />
                 <h1 className="text-xl font-bold">InSight</h1>
             </div>
         </header>
