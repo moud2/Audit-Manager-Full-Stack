13154cc3a52827ed606a8bd1714e55e5e077e539
Matthes Klemt <matthes.klemt@stud.htwk-leipzig.de>
2024-06-11 17:10:57 +0200

    fix-18: edited categorycontroller and auditcontroller tests

---
+++ src/test/java/com/insight/backend/controller/CategoryControllerTests.java
@@ -0,0 +1,42 @@
+package com.insight.backend.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.insight.backend.model.Category;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+
+public class CategoryControllerTests {
+
+    @Autowired
+	private MockMvc mockMvc;
+	
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+	public void testGetCategroies() throws Exception {
+		List<Category> response = new ArrayList<>();
+        response.add(new Category(0, "a"));
+        response.add(new Category(1, "b"));
+        response.add(new Category(2, "c"));
+
+		String expectedJson = objectMapper.writeValueAsString(response);
+
+		mockMvc.perform(get("/api/v1/categories")).andExpect(status().isOk()).andExpect(content().json(expectedJson));
+
+	}
+}
