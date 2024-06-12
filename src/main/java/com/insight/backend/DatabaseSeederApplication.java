4525f032886cf8221cf9051c86d024822f6175f1
Robin Kehl <robin.kehl@singular-it.de>
2024-06-12 13:54:33 +0200

    feat: add empty gradle task for database seeding

---
+++ src/main/java/com/insight/backend/DatabaseSeederApplication.java
@@ -0,0 +1,15 @@
+package com.insight.backend;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseSeederApplication {
+
+    public static void main(String[] args) {
+        // Ihre Seeding-Logik hier
+        System.out.println("Seeding the database...");
+        // logic here
+        System.out.println("Database seeded.");
+    }
+
+}
