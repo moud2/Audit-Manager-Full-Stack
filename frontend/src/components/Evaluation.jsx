ffb47a77f4161a044d99044eae89b21ccc2b6584
Pascal Henrici <pascal.henrici@stud.htwk-leipzig.de>
2024-06-13 17:33:25 +0200

    feat: import dependecies

--- frontend/src/components/Evaluation.jsx
+++ frontend/src/components/Evaluation.jsx
@@ -1,37 +1,41 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import {CircularProgressWithLabel, LinearProgressWithLabel} from '@mui/material';
 
 function Evaluation() {
 
-    const evaluationData = {
-        categories: [
-            {
-                name: "Category 1",
-                questions: [
-                    {question: "Question 1.1", rating: 4},
-                    {question: "Question 1.2", rating: 1},
+    let progress = 70;
+    let amountQuestions = 25;
+    let amountCategories = 5;
 
-                ]
-            },
-            {
-                name: "Category 2",
-                questions: [
-                    {question: "Question 2.1", rating: 5},
-                    {question: "Question 2.2", rating: 2},
+    let categorie1 = [3, 5, 2, 1, 0, 0, 4, 5, 3];
 
-                ]
-            }
-        ]
-    };
 
+    let categorie2 = [2, 5, 5, 1, 1, 3, 4, 5, 3];
+    let categorie3 = [0, 1, 2, 1, 0, 0, 2, 5, 3];
+    let categorie4 = [5, 5, 2, 1, 5, 0, 2, 5, 4];
+    let categorie5 = [3, 5, 2, 4, 0, 2, 4, 5, 3];
 
     return (
+        <div>
+            <div id="title">
+                <h1 className="text-center text-4xl m-10">Dashboard</h1>
+            </div>
 
+            <div id="result">
+                <LinearProgressWithLabel value={progress}/>
+            </div>
 
-        <h2>Bewertung</h2>
+            <div id="categories">
+                <CircularProgressWithLabel value={progress}/>
+            </div>
 
+            <div id="result_per_question">
 
-    );
+            </div>
+        </div>
+
+    )
 }
 
 export default Evaluation;
