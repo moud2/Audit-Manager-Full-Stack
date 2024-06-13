c15d5d1f6f3dc2c9c5dd4496e189ef3c93c09343
Pascal Henrici <pascal.henrici@stud.htwk-leipzig.de>
2024-06-13 12:39:01 +0200

    feat: add dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,41 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from 'react-router-dom';
+import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
 import Dashboard from './components/Dashboard.jsx';
 import NewAudit from './components/NewAudit.jsx';
 import PerformAudit from './components/PerformAudit.jsx';
 import Evaluation from './components/Evaluation.jsx';
+import './index.css';
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Dashboard</Link>
-            </li>
-            <li>
-              <Link to="/newAudit">NewAudit</Link>
-            </li>
-            <li>
-              <Link to="/performAudit">PerformAudit</Link>
-            </li>
-            <li>
-              <Link to="/evaluation">Evaluation</Link>
-            </li>
-          </ul>
-        </nav>
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Dashboard</Link>
+                        </li>
+                        <li>
+                            <Link to="/newAudit">NewAudit</Link>
+                        </li>
+                        <li>
+                            <Link to="/performAudit">PerformAudit</Link>
+                        </li>
+                        <li>
+                            <Link to="/evaluation">Evaluation</Link>
+                        </li>
+                    </ul>
+                </nav>
 
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/newAudit" element={<NewAudit />} />
-          <Route path="/performAudit" element={<PerformAudit />} />
-          <Route path="/evaluation" element={<Evaluation />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+                <Routes>
+                    <Route path="/" element={<Dashboard/>}/>
+                    <Route path="/newAudit" element={<NewAudit/>}/>
+                    <Route path="/performAudit" element={<PerformAudit/>}/>
+                    <Route path="/evaluation" element={<Evaluation/>}/>
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
