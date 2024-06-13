ad3ddbfdbea6062ed39f41498a23c7c7210a831d
Pascal Henrici <pascal.henrici@stud.htwk-leipzig.de>
2024-06-13 16:46:10 +0200

    safe commit

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import Paper from '@mui/material/Paper';
 import AddIcon from '@mui/icons-material/Add';
@@ -10,7 +11,7 @@ function Dashboard() {
             <div>
                 <h1 className="text-center text-4xl m-10">Dashboard</h1>
             </div>
-            
+
             <div className="grid grid-cols-4 gap-4 justify-center">
                 <Link to="/newAudit" className="testbox flex justify-center items-center h-full">
                     <Paper elevation={8} className="w-60 h-60 flex justify-center items-center">
--- frontend/src/components/Evaluation.jsx
+++ frontend/src/components/Evaluation.jsx
@@ -1,7 +1,37 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 
 function Evaluation() {
-  return <h2>Bewertung</h2>;
+
+    const evaluationData = {
+        categories: [
+            {
+                name: "Category 1",
+                questions: [
+                    {question: "Question 1.1", rating: 4},
+                    {question: "Question 1.2", rating: 1},
+
+                ]
+            },
+            {
+                name: "Category 2",
+                questions: [
+                    {question: "Question 2.1", rating: 5},
+                    {question: "Question 2.2", rating: 2},
+
+                ]
+            }
+        ]
+    };
+
+
+    return (
+
+
+        <h2>Bewertung</h2>
+
+
+    );
 }
 
 export default Evaluation;
