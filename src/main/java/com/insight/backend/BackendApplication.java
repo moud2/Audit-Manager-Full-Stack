8c2ddec02d113ee0c236044382b8c1168a9bc5df
Robert Eikmanns <robert.eikmanns@stud.htwk-leipzig.de>
2024-06-15 18:57:59 +0200

    correct package

--- src/main/java/com/insight/backend/BackendApplication.java
+++ src/main/java/com/insight/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.insight.backend.controller;
+package com.insight.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
