36308d93020b8b9d9b7e06d6923549b0d7e8fa0d
Amani Hamad <amani.hamad@stud.htwk-leipzig.de>
2024-06-18 12:30:37 +0200

    feat: Add flexible CORS configuration using environment properties

--- src/main/java/com/insight/backend/config/WebConfig.java
+++ src/main/java/com/insight/backend/config/WebConfig.java
@@ -1,23 +1,40 @@
 package com.insight.backend.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Configuration class for setting up CORS (Cross-Origin Resource Sharing) settings.
+ */
 @Configuration
 public class WebConfig {
 
+    @Autowired
+    private Environment env;
+
+    /**
+     * Configures CORS settings based on application properties.
+     * 
+     * @return a WebMvcConfigurer instance with CORS settings.
+     */
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+                // Retrieve the allowed origins from the application properties. If not set, default to "*".
+                String allowedOrigins = env.getProperty("cors.urls", "*");
+                
+                // Configure CORS settings
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE",  "PATCH", "OPTIONS") // Specifies allowed HTTP methods
-                        .allowedHeaders("*") // Allows all headers in requests
-                        .allowCredentials(true); // Enables credentials (cookies, authorization headers, etc.) to be sent in requests
+                        .allowedOrigins(allowedOrigins.split(","))
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
