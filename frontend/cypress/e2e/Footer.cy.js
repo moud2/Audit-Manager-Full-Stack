c6af87260d8ff6b32fb6b1d1a8a6e1d678d5c424
Raad <saif.raadsh1998@gmail.com>
2024-06-18 16:22:50 +0200

    reform: Footer test.cy.js name changed

--- frontend/cypress/e2e/Footer.cy.js
+++ frontend/cypress/e2e/Footer.cy.js
@@ -1,5 +1,5 @@
-describe('First test', () => {
-  it('Does not do much', () => {
+describe('Footer test', () => {
+  it('Visits InSight (Localhost)', () => {
     cy.visit('http://localhost:5173/')
     cy.get('footer')
     cy.get('.text-sm') 
