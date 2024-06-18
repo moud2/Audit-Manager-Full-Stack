3234819b1385e709f5dc333f53601cd7840efbf5
Raad <saif.raadsh1998@gmail.com>
2024-06-18 16:22:50 +0200

    reform: Footer test.cy.js name changed

--- frontend/src/Index.jsx
+++ frontend/src/Index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
