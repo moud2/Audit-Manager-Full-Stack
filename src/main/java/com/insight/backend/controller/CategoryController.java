5730f0f489eaba8663dc7ebf41bba26edd162df7
Janis Kupfer <janis.kupfer@stud.htwk-leipzig.de>
2024-06-19 12:06:31 +0200

    fix: merge main into local repository

--- src/main/java/com/insight/backend/controller/CategoryController.java
+++ src/main/java/com/insight/backend/controller/CategoryController.java
@@ -1,31 +1,30 @@
 package com.insight.backend.controller;
 
-
-import com.insight.backend.model.Category;
-import com.insight.backend.service.CategoryService;
-
-import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import com.insight.backend.model.Category;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
 @RestController
 public class CategoryController {
-
-    private final CategoryService categoryService;
-
-    @Autowired
-    public CategoryController(CategoryService categoryService) {
-        this.categoryService = categoryService;
-    }
 
     /*GET request for categories with placeholder data */
     @GetMapping("/api/v1/categories")
     public ResponseEntity<List<Category>> getCategory() {
-        List<Category> response = categoryService.getAllCategories();
+        List<Category> response = new ArrayList<>();
+        Category category1 = new Category("a", Set.of());
+        Category category2 = new Category("b", Set.of());
+        Category category3 = new Category("c", Set.of());
+        category1.setId((long) 1);
+        category2.setId((long) 2);
+        category3.setId((long) 3);
+        response.add(category1);
+        response.add(category2);
+        response.add(category3);
         return ResponseEntity.ok(response);
     }
-
 }
