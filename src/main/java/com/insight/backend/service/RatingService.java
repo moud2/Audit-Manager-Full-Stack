7f89d924305ee6204434cab6f9ac61e21a825fb9
Matthias Lauer <matthias.lauer@stud.htwk-leipzig.de>
2024-06-19 12:31:01 +0200

    fix: order of import in RatingService and RatingRepository

--- src/main/java/com/insight/backend/service/RatingService.java
+++ src/main/java/com/insight/backend/service/RatingService.java
@@ -1,24 +1,21 @@
 package com.insight.backend.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 import com.insight.backend.model.Rating;
 import com.insight.backend.repository.RatingRepository;
 
 @Service
 public class RatingService {
-
-    private final RatingRepository ratingRepository;
-
+/*
+ 
     @Autowired
-    public RatingService(RatingRepository ratingRepository) {
-        this.ratingRepository = ratingRepository;
-    }
+    private RatingRepository ratingRepository;
 
     public List<Rating> getAllRatings() {
         return ratingRepository.findAll();
-    }
+    }*/
 }
