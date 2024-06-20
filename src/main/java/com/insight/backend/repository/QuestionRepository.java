2b11b3db7df451e2b16b6c3d9daf7cc8efb2b14f
Anass Boustani <anassboustani01@gmail.com>
2024-06-20 22:30:47 +0200

    DAO und ServiceClass erstellen

--- src/main/java/com/insight/backend/repository/QuestionRepository.java
+++ src/main/java/com/insight/backend/repository/QuestionRepository.java
@@ -1,7 +1,6 @@
 package com.insight.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.insight.backend.model.Question;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
--- src/main/java/com/insight/backend/service/QuestionService.java
+++ src/main/java/com/insight/backend/service/QuestionService.java
@@ -1,12 +1,10 @@
 package com.insight.backend.service;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.insight.backend.model.Question;
 import com.insight.backend.repository.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class QuestionService {
