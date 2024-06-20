b9f9f5bb9f2d52bd8cadea53b57502099ffeb6b5
Benedikt Meyer <benedikt.meyer@stud.htwk-leipzig.de>
2024-06-20 14:52:46 +0200

    fix: kicked Constructor out of AuditService and replaced with Dependency Injection

--- src/main/java/com/insight/backend/service/AuditService.java
+++ src/main/java/com/insight/backend/service/AuditService.java
@@ -11,12 +11,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuditService {
 
-    private final AuditRepository auditRepository;
-
     @Autowired
-    public AuditService(AuditRepository auditRepository) {
-        this.auditRepository = auditRepository;
-    }
+    private final AuditRepository auditRepository;
 
     public List<Audit> getAllAudits() {
         return auditRepository.findAll();
