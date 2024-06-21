47c39b5a18dc5f1ec667826f549bf5ea2e8cfc41
Robert Eikmanns <robert.eikmanns@stud.htwk-leipzig.de>
2024-06-21 21:18:27 +0200

    refactor remove blank

--- src/main/java/com/insight/backend/repository/QuestionRepository.java
+++ src/main/java/com/insight/backend/repository/QuestionRepository.java
@@ -4,6 +4,5 @@ import com.insight.backend.model.Question;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 }
