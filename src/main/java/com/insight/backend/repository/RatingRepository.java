bcbe329a3b6f0d7e85ca644c75e766a1f7f74620
Robert Eikmanns <robert.eikmanns@stud.htwk-leipzig.de>
2024-06-21 21:01:45 +0200

    refactor: change order of import statements

--- src/main/java/com/insight/backend/repository/RatingRepository.java
+++ src/main/java/com/insight/backend/repository/RatingRepository.java
@@ -1,8 +1,8 @@
 package com.insight.backend.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.insight.backend.model.Rating;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RatingRepository extends JpaRepository<Rating, Long> {
 }
