ce03e17e3bf4b3494eaae042de2ff4b641850678
Janis Kupfer <janis.kupfer@stud.htwk-leipzig.de>
2024-06-27 16:54:37 +0200

    fix: capitalize names of directories

--- src/main/java/com/insight/backend/controller/CategoryController.java
+++ src/main/java/com/insight/backend/controller/CategoryController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Set;
 
 import com.insight.backend.model.Category;
+import com.insight.backend.service.Category.FindCategoryService;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
@@ -12,19 +13,12 @@ import org.springframework.http.ResponseEntity;
 @RestController
 public class CategoryController {
 
+    private FindCategoryService findCategoryService = new FindCategoryService();
+
     /*GET request for categories with placeholder data */
     @GetMapping("/api/v1/categories")
     public ResponseEntity<List<Category>> getCategory() {
-        List<Category> response = new ArrayList<>();
-        Category category1 = new Category("a", Set.of());
-        Category category2 = new Category("b", Set.of());
-        Category category3 = new Category("c", Set.of());
-        category1.setId((long) 1);
-        category2.setId((long) 2);
-        category3.setId((long) 3);
-        response.add(category1);
-        response.add(category2);
-        response.add(category3);
+        List<Category> response = findCategoryService.findAllCategories();
         return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/insight/backend/service/DatabaseSeederService.java
+++ src/main/java/com/insight/backend/service/DatabaseSeederService.java
@@ -10,6 +10,7 @@ import java.util.Map;
 
 import com.insight.backend.service.Category.SaveCategoryService;
 import com.insight.backend.service.Question.SaveQuestionService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
