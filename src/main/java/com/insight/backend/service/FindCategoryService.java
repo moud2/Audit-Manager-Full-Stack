8843f05e2b609f844bd7c90694f2a71d798d0fb7
Amani Hamad <amani.hamad@stud.htwk-leipzig.de>
2024-06-27 21:48:06 +0200

    feat: add getCategoryRepository methode to class FindCategoryService and JavaDoc

--- src/main/java/com/insight/backend/service/FindCategoryService.java
+++ src/main/java/com/insight/backend/service/FindCategoryService.java
@@ -6,17 +6,40 @@ import org.springframework.stereotype.Service;
 import com.insight.backend.model.Category;
 import com.insight.backend.repository.CategoryRepository;
 
+/**
+ * Service-class to find categpries in the database.
+ */
 @Service
 public class FindCategoryService {
 
-    private  CategoryRepository categoryRepository;
+    private final  CategoryRepository categoryRepository;
 
     
+    /**
+     * construktor to injekt the CategoryRepository.
+     *
+     * @param categoryRepository the repository  for accessing auf category data.
+     */
     public FindCategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
     }
 
+    /**
+     * finds a category based on the given id.
+     *
+     * @param id the id of category to be found.
+     * @return an Optional-Objekt, that includes the category, if finded or empty if not funded.
+     */
     public Optional<Category> findCategoryById(Long id) {
         return categoryRepository.findById(id);
+    }
+
+    /**
+     * Gets the CategoryRepository.
+     * 
+     * @return the category repository
+     */
+    public CategoryRepository getCategoryRepository() {
+        return categoryRepository;
     }
 }
