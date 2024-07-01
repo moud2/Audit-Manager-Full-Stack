339e766ea4db7ee06ebc4ad9d32274cb82dc46e4
bmeyer1 <benedikt.meyer@stud.htwk-leipzig.de>
2024-07-01 12:01:38 +0200

    fix: Java Docs

--- src/main/java/com/insight/backend/service/Rating/SaveRatingService.java
+++ src/main/java/com/insight/backend/service/Rating/SaveRatingService.java
@@ -5,19 +5,41 @@ import java.util.*;
 import com.insight.backend.repository.RatingRepository;
 import com.insight.backend.model.Rating;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * SaveRatingService is a service, which is responsible for storing rating objects in the database.
+ */
 @Service
 public class SaveRatingService {
 
-    @Autowired
-    private RatingRepository ratingRepository;
+    final RatingRepository ratingRepository;
 
+    /**
+     * constructor
+     * 
+     * @param ratingRepository is an instance of RatingRepository used for JpaRepository operations.
+     */
+    public SaveRatingService(RatingRepository ratingRepository) {
+        this.ratingRepository = ratingRepository;
+    }
+
+    /**
+     * Stores a single rating object in the database.
+     * 
+     * @param rating the rating objekt to be saved.
+     * @return the saved rating object.
+     */
     public Rating saveRating(Rating rating) {
         return ratingRepository.save(rating);
     }
 
+    /**
+    * Stores multiple rating objects in the database.
+    * 
+    * @param ratingList is a list of rating objects to save.
+    * @return the saved list of rating objects.
+    */
     public List<Rating> saveAllRatings(List<Rating> ratingList) {
         ratingRepository.saveAll(ratingList);
         return ratingList;
