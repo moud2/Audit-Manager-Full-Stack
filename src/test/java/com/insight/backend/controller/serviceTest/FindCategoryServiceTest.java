d417c676fd2896881c78a5f2d186db96c6e4ca16
Amani Hamad <amani.hamad@stud.htwk-leipzig.de>
2024-07-01 12:30:05 +0200

    docs: Add documentation to FindCategoryServiceTest

--- src/test/java/com/insight/backend/controller/serviceTest/FindCategoryServiceTest.java
+++ src/test/java/com/insight/backend/controller/serviceTest/FindCategoryServiceTest.java
@@ -1,4 +1,5 @@
 package com.insight.backend.controller.serviceTest;
+
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -16,39 +17,49 @@ import com.insight.backend.model.Category;
 import com.insight.backend.repository.CategoryRepository;
 import com.insight.backend.service.Category.FindCategoryService;
 
+/**
+ * Test class for FindCategoryService.
+ */
 @ExtendWith(MockitoExtension.class)
 public class FindCategoryServiceTest {
+
     @Mock
     private CategoryRepository categoryRepository;
+
     @InjectMocks
     private FindCategoryService findCategoryService;
 
     private Category category1;
-    private Category category2;
 
+    /**
+     * Set up method to initialize test data.
+     */
     @BeforeEach
-    void setUp(){
+    void setUp() {
         category1 = new Category();
         category1.setId(1L);
         category1.setName("Category1");
     }
 
+    /**
+     * Test case for finding a category by ID when the category is found.
+     */
     @Test
     void testFindCategoryById_found() {
+        // Arrange: Set up mock behavior for categoryRepository
         when(categoryRepository.findById(1L)).thenReturn(Optional.of(category1));
-
         Category foundCategory = findCategoryService.findCategoryById(1L);
-
         assertEquals(category1, foundCategory);
     }
 
+    /**
+     * Test case for finding a category by ID when the category is not found.
+     */
     @Test
-    void testFindAuditById_notFound() {
+    void testFindCategoryById_notFound() {
+        // Arrange: Set up mock behavior for categoryRepository
         when(categoryRepository.findById(anyLong())).thenReturn(Optional.empty());
-
         Category foundCategory = findCategoryService.findCategoryById(1L);
-
         assertNull(foundCategory);
     }
-
 }
