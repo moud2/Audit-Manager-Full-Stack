7c8ef8cb8e1a5f345f3199d8fd333913c0af5ae1
Anna Liepelt <anna.liepelt@stud.htwk-leipzig.de>
2024-07-04 11:17:40 +0200

    feat: add PATCH request

--- frontend/src/components/PerformAudit.jsx
+++ frontend/src/components/PerformAudit.jsx
@@ -3,6 +3,7 @@ import { FormGroup, FormControlLabel, Checkbox, Button } from '@mui/material';
 import { TextareaAutosize as BaseTextareaAutosize } from '@mui/base/TextareaAutosize';
 import { styled } from '@mui/system';
 import api from "../api.js";
+import { useParams } from "react-router-dom";
 
 //Styling for the Comment Sections
 const Textarea = styled(BaseTextareaAutosize)(
@@ -34,7 +35,7 @@ const Textarea = styled(BaseTextareaAutosize)(
 );
 
 /**
- * Creates the perform audits site with basic functionality
+ * Creates the perform audits site, which shows the questions, checkboxes for the rating and a comment section. 
  * 
  * @author [Anna Liepelt] https://gitlab.dit.htwk-leipzig.de/anna.liepelt
  */
@@ -44,9 +45,12 @@ function PerformAudit() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  // const { auditId } = useParams();
+  const auditId = 1;
+
   /*fetching data from the backend*/
   useEffect(() => {
-    api.get('http://localhost:8080/api/v1/audits/1/ratings') /*relative path, editable in .env files & api.js -*/
+    api.get(`/v1/audits/${auditId}/ratings`)
         .then(response => {
             console.log(response);
             setQuestions(response.data);
@@ -59,24 +63,64 @@ function PerformAudit() {
             });
     }, []);
 
+  // if (loading) {
+  //   return <p>Laden...</p>;
+  // }
+
+  // if (error) {
+  //   return <p>Fehler: {error.message}</p>;
+  // }
 
   const updateQuestionById = (id, newPartialQuestion) => {
     const q = questions.map(question => id === question.id? {...question, ...newPartialQuestion} : question );
     setQuestions(q);
   }
 
+  /*PATCH function for sending changes to the backend */
+  const patchQuestion = async (ratingId, newRatings) => {
+    const patchData = newRatings.map((destination) => ({
+      op: "replace",
+      path: `${destination.path}`,
+      value: destination.value,
+    }));
+    console.log(patchData);
+    api.patch(`/v1/ratings/${ratingId}`, patchData)
+    .then(response => {
+      console.log(response);
+      setLoading(false);
+      })
+      .catch(err => {
+          console.error('Error fetching data:', err);
+          setError(err);
+          setLoading(false);
+      });
+      
+  };
+
   const handleCommentInput = (event, id) => {
     const newComment = event.target.value;
-    updateQuestionById(id, {comment: newComment})
+    updateQuestionById(id, {comment: newComment});
+    patchQuestion(id, [{path: "/comment", value: newComment}]);
   };
 
   const handleCheckboxChange = (event, label, question) => {
+    const isChecked = event.target.checked
+
+    if(!isChecked) {
+      const newQuestion = {na: null, points: null}
+      updateQuestionById(question.id, newQuestion);
+      patchQuestion(question.id, [{path: "/na", value: newQuestion.na}, {path: "/points", value: newQuestion.points}]);
+      return
+    }
+
     switch (label) {
       case 'N/A':
-        updateQuestionById(question.id, {na: !question.na, points: null});
+        updateQuestionById(question.id, {na: true, points: null});
+        patchQuestion(question.id, [{path: "/na", value: true}, {path: "/points", value: null}]);
         break;
       default:
-        updateQuestionById(question.id, {points: label == question.points? null : label, na: false});
+        updateQuestionById(question.id, {points: label, na: false});
+        patchQuestion(question.id, [{path: "/na", value: false}, {path: "/points", value: label}]);
     }
   };
 
@@ -94,7 +138,7 @@ function PerformAudit() {
       <h1 className="px-10 py-5 font-bold">Audit durchführen</h1>
       {questions.map((question) => (
         <div key={question.id}>
-          <h2 className="px-10 py-5">{question.question}</h2>
+          <h2 className="px-10 py-5">{question.question} {question.id}</h2>
           <FormGroup className="px-5 flex justify-center" row>
             {[0, 1, 2, 3, 4, 5, 'N/A'].map((label) => (
               <FormControlLabel
@@ -117,9 +161,6 @@ function PerformAudit() {
           />
         </div>
       ))}
-      <Button 
-        //ToDo: Add onClick Eventlistener
-      >Speichern</Button>
     </>
   );
 }
