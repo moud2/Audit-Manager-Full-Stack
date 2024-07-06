7d7f09ce43cb3070484ded16ecf1b7b783f48496
Matthias Lauer <matthias.lauer@stud.htwk-leipzig.de>
2024-07-06 14:16:07 +0200

    fix: add missing comments in javadoc style

--- src/main/java/com/insight/backend/controller/CategoryController.java
+++ src/main/java/com/insight/backend/controller/CategoryController.java
@@ -11,18 +11,31 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
+/**
+ * CategoryController is a REST controller that handles HTTP requests related to categories.
+ */
 @RestController
 public class CategoryController {
-    /* FindCategoryService to use the service methods */
+    /** 
+     * The FindCategoryService to use the service methods.
+     */
     private final FindCategoryService findCategoryService;
 
-    /* Constructor with FindCategoryService */
+    /**
+     * Constructs a new CategoryController with the specified FindCategoryService.
+     * 
+     * @param findCategoryService the service to find categories
+     */
     @Autowired
     public CategoryController(FindCategoryService findCategoryService) {
         this.findCategoryService = findCategoryService; 
     }
 
-    /* GET request for categories from Service class */
+    /**
+     * GET requests for retrieving all categories.
+     * 
+     * @return a ResponseEntity containing a list of Category objects
+     */
     @GetMapping("/api/v1/categories")
     public ResponseEntity<List<Category>> getCategory() {
         List<Category> response = findCategoryService.findAllCategories();
