d2334f3e3e88f0260375f06504a69b9a041e69c8
Anass Boustani <anassboustani01@gmail.com>
2024-07-10 17:28:57 +0200

    sidebar reduzieren

--- frontend/cypress/e2e/Sidebar.cy.js
+++ frontend/cypress/e2e/Sidebar.cy.js
@@ -8,20 +8,7 @@ describe('Sidebar Navigation', () => {
         cy.get('[data-cy="nav-dashboard"]').click()
 
         cy.url().should('eq', 'http://localhost:5173/');
-
-        cy.get('[data-cy="nav-newAudit"]').click()
-
-        cy.url().should('eq', 'http://localhost:5173/newAudit');
-
-        cy.get('[data-cy="nav-performAudit"]').click()
-
-        cy.url().should('eq', 'http://localhost:5173/performAudit');
-
-        cy.get('[data-cy="nav-evaluation"]').click()
-
-        cy.url().should('eq', 'http://localhost:5173/evaluation');
-
-
+  
     });
 });
   
