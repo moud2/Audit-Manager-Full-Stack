e25042492af3a78a7b532e89f5996a2f5f1dc64e
Raad <saif.raadsh1998@gmail.com>
2024-07-11 17:06:01 +0200

    feat: darkmode removed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 
 const Footer = () => {
-    return (
-        <footer
-            className="fixed flex justify-center items-center bottom-0 h-16 w-full bg-white border-t border-gray-200 shadow dark:bg-gray-800 dark:border-gray-600">
-      <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-        © 2024{' '}
-          <a href="http://localhost:5173/" className="hover:underline">
+  return (
+    <footer className="fixed flex justify-center items-center bottom-0 h-16 w-full bg-white border-t border-gray-200 shadow">
+      <span className="block text-sm text-gray-500 sm:text-center">
+        © 2024{" "}
+        <a href="http://localhost:5173/" className="hover:underline">
           InSight
-        </a>{' '}
-          All Rights Reserved.
+        </a>{" "}
+        All Rights Reserved.
       </span>
-        </footer>
-    );
+    </footer>
+  );
 };
 
 export default Footer;
