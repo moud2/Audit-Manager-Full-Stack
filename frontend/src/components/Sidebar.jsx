081ce94d437d0d60f4cff0a5e0a751cda853faea
Philipp Voigt <philipp.voigt@stud.htwk-leipzig.de>
2024-07-11 14:16:53 +0200

    refactor: style changes

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 const Sidebar = () => {
     return (
         <div className="w-64 h-[calc(100vh-4rem-4.5rem)] bg-gray-200 p-4 shadow-md fixed">
-                    <Link data-cy="nav-dashboard" to="/" className="block p-2 mb-4 text-gray-700 hover:text-blue-500 border border-gray-400 rounded text-center">Dashboard</Link>
+                    <Link data-cy="nav-dashboard" to="/" className="block p-2 mt-4 text-gray-700 hover:text-black hover:scale-105 transition-transform transform border border-gray-400 rounded text-center">Dashboard</Link>
         </div>
     );
 }
