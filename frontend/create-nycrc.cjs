278ce7c4a8d40a4784cd1b26e86fc1aeef23b0b6
Abolfazl <abolfazl.shahbazfar@stud.htwk-leipzig.de>
2024-07-17 02:43:07 +0200

    using coverage reports

--- frontend/create-nycrc.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const nycrcContent = {
-  include: ["instrumented/*.js"],
-  exclude: ["cypress/e2e/*.js"],
-  reporter: ["lcov", "text"]
-};
-fs.writeFileSync('.nycrc', JSON.stringify(nycrcContent, null, 2));
-console.log('.nycrc file created');
