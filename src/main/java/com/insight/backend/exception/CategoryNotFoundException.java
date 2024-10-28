48743acceb9c2f55d68c56705dc46fe0a40d925a
Amani Hamad <amani.hamad@stud.htwk-leipzig.de>
2024-10-28 17:50:49 +0100

    add CategoryNotFoundExeption file to exception folder

---
+++ src/main/java/com/insight/backend/exception/CategoryNotFoundException.java
@@ -0,0 +1,15 @@
+package com.insight.backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+/**
+ * Exception thrown when a requested category is not found.
+ * This exception results in an HTTP status code of 404 (NOT FOUND).
+ */
+@ResponseStatus(value=HttpStatus.NOT_FOUND, reason="No such Category")                                                                                                           
+public class CategoryNotFoundException extends RuntimeException {
+
+    public CategoryNotFoundException(String par) {
+    }
+    
+}
