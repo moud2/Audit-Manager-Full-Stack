ffe107c9d07676f5d625d28d6939b384a29b99fa
Anna Liepelt <anna.liepelt@stud.htwk-leipzig.de>
2024-10-28 10:56:45 +0100

    refactor-181: add CheckboxSelect component

--- frontend/src/components/QuestionList/CheckboxSelect.jsx
+++ frontend/src/components/QuestionList/CheckboxSelect.jsx
@@ -1,3 +1,32 @@
-export function CheckboxSelect() {
+import {Checkbox, FormControlLabel, FormGroup} from "@mui/material";
+import React, {useState} from "react";
 
+export function CheckboxSelect({ value, options, onChange }) {
+
+    const handleCheckboxChange = (event, label) => {
+        const isChecked = event.target.checked;
+
+        if (!isChecked) {
+            onChange(null);
+        } else {
+            onChange(label);
+        }
+    }
+
+    return (
+        <FormGroup className="px-5 flex justify-center" row>
+            {options.map((label) => (
+                <FormControlLabel
+                    key={label}
+                    control={
+                        <Checkbox
+                            checked={value === label}
+                            onChange={(event) => handleCheckboxChange(event, label) }
+                        />
+                    }
+                    label={label.toString()}
+                />
+            ))}
+        </FormGroup>
+    )
 }
