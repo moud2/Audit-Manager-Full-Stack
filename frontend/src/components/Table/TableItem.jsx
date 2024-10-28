976b071f1550814d98ddabc5673b80c9af023a45
Abolfazl <abolfazl.shahbazfar@stud.htwk-leipzig.de>
2024-10-28 17:05:56 +0100

    Tabelle mit beispieldaten

---
+++ frontend/src/components/Table/TableItem.jsx
@@ -0,0 +1,14 @@
+// src/components/Table/TableItem.jsx
+import React from "react";
+
+export default function TableItem({ id, title, onDragStart }) {
+    return (
+        <div
+            draggable="true"
+            onDragStart={(e) => onDragStart(e, id)}
+            className="cursor-grab rounded border bg-neutral-700 p-3 text-sm text-neutral-100"
+        >
+            {title}
+        </div>
+    );
+}
