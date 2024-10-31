ac903bb202175b0e76ac68155cc936f00410051d
Raad <saif.raadsh1998@gmail.com>
2024-10-31 18:33:10 +0100

    PlusIcon renamed and docs has been written

--- frontend/src/components/AuditGrid/AuditGridItem.jsx
+++ frontend/src/components/AuditGrid/AuditGridItem.jsx
@@ -1,16 +1,21 @@
-// src/components/AuditGrid/AuditGridItem.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function AuditGridItem({ audit }) {
+/**
+ * AuditGridItem component renders a clickable grid item that links to an audit.
+ *
+ * @param {object} audit - The audit object containing `id` and `name`.
+ * @returns {JSX.Element} The rendered grid item component.
+ * @constructor
+ */
+export function AuditGridItem({ audit }) {
     return (
         <Link 
             data-cy="data-buttons" 
             to={`/performAudit/${audit.id}`} 
-            className="flex justify-center w-44 lg:w-52 xl:w-52 items-center aspect-square transition-transform transform hover:scale-105 bg-gray-200 rounded-xl m-4 p-4 border border-gray-400">
+            className="flex justify-center w-44 lg:w-52 xl:w-52 items-center aspect-square transition-transform transform hover:scale-105 bg-gray-200 rounded-xl m-4 p-4 border border-gray-400"
+        >
             <p className="text-center">{audit.name}</p>
         </Link>
     );
 }
-
-export default AuditGridItem;
--- frontend/src/components/AuditGrid/NewAuditGridItem.jsx
+++ frontend/src/components/AuditGrid/NewAuditGridItem.jsx
@@ -20,4 +20,3 @@ export function NewAuditGridItem() {
         </Link>
     );
 }
-
