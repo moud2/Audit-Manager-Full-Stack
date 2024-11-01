465af11ff73a521eb7a5ba160a890c0fb442d931
Robin Kehl <robin.kehl@singular-it.de>
2024-11-01 18:20:31 +0100

    docs(audit): add AuditSpecifications doc string

--- src/main/java/com/insight/backend/specifications/AuditSpecifications.java
+++ src/main/java/com/insight/backend/specifications/AuditSpecifications.java
@@ -5,27 +5,43 @@ import com.insight.backend.model.Audit;
 import org.springframework.data.jpa.domain.Specification;
 
 public class AuditSpecifications {
+    /**
+     * Specification to find an Audit by its name.
+     *
+     * @param name the exact name of the Audit
+     * @return the specification to find the Audit
+     */
     public static Specification<Audit> hasName(String name) {
         return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("name"), name);
     }
 
+    /**
+     * Specification to find an Audit by its customer.
+     *
+     * @param customer the exact name of the customer
+     * @return the specification to find the Audit
+     */
     public static Specification<Audit> hasCustomer(String customer) {
         return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("customer"), customer);
     }
 
-    // customer starts with
+    /**
+     * Specification to find an Audit by its customer.
+     *
+     * @param customer the start of the name of the customer
+     * @return the specification to find the Audit
+     */
     public static Specification<Audit> customerStartsWith(String customer) {
         return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get("customer"), customer + "%");
     }
 
-    // customer contains
+    /**
+     * Specification to find an Audit by its customer.
+     *
+     * @param customer the name of the customer
+     * @return the specification to find the Audit
+     */
     public static Specification<Audit> customerContains(String customer) {
         return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get("customer"), "%" + customer + "%");
     }
-//    public static Specification<Audit> hasCategory(Long categoryId) {
-//        return (root, query, criteriaBuilder) -> {
-//            Join<Audit, Category> join = root.join("categories");
-//            return criteriaBuilder.equal(join.get("id"), categoryId);
-//        };
-//    }
 }
