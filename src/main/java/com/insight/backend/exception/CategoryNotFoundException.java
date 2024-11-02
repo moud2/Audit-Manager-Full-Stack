0b5178f3ca65441b88413f62f1d03198dd52caad
Mahamoud <mahamoud_hafed.mohamed@stud.htwk-leipzig.de>
2024-11-02 17:15:57 +0100

    refac: changed the http response to bad request

--- src/main/java/com/insight/backend/exception/CategoryNotFoundException.java
+++ src/main/java/com/insight/backend/exception/CategoryNotFoundException.java
@@ -1,16 +1,17 @@
 package com.insight.backend.exception;
 
+// This exception should result in a 400 Bad Request HTTP response
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * Exception thrown when a requested category is not found.
- * This exception results in an HTTP status code of 404 (NOT FOUND).
+ * This exception results in an HTTP status code of 400 (BAD REQUEST).
  */
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No such Category")
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "No such Category")
 public class CategoryNotFoundException extends RuntimeException {
 
     public CategoryNotFoundException(String message) {
-        super(message); // Ruft den Konstruktor der RuntimeException auf, um die Fehlermeldung zu setzen
+        super(message); // Calls the constructor of RuntimeException to set the error message
     }
 }
