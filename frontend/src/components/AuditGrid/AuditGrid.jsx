93ff1a74d0202acbdcc746a244819f0c9a04d6a6
Raad <saif.raadsh1998@gmail.com>
2024-11-07 18:27:24 +0100

    AuditGrid changed

--- frontend/src/components/AuditGrid/AuditGrid.jsx
+++ frontend/src/components/AuditGrid/AuditGrid.jsx
@@ -1,3 +1,51 @@
-export function AuditGrid(){
+import React, { useEffect, useState } from "react";
+import api from "../../api";
+import { AuditGridItem } from "./AuditGridItem";
+import { NewAuditGridItem } from "./NewAuditGridItem";
 
+/**
+ * AuditGrid component fetches audit data from the backend and displays
+ * them in a grid format, including a button to create a new audit.
+ *
+ * @returns {JSX.Element} The rendered audit grid component.
+ * @constructor
+ */
+export function AuditGrid() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    api
+      .get("/v1/audits")
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("Error fetching data:", err);
+        setError(err);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Laden...</p>;
+  }
+
+  return (
+    <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-10 justify-center">
+        {/* PlusIcon */}
+        <NewAuditGridItem />
+
+        {/* Existing Audits */}
+        {data.map((audit) => (
+          <AuditGridItem key={audit.id} audit={audit} />
+        ))}
+      </div>
+    </div>
+  );
 }
+
+export default AuditGrid;
---
+++ frontend/src/components/AuditGrid/doc.md
@@ -0,0 +1,25 @@
+# MyExampleComponent
+
+## Props
+
+### title
+
+Dashboard
+
+### content
+
+- Type: `function`
+
+Displays Audit Items
+
+New Audit Button (Plus Icon)
+
+## Usage
+
+### Basic usage
+
+```jsx
+<AuditGrid 
+   
+></AuditGrid>
+``` 
