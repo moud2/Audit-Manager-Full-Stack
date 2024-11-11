1eb308dcc95584af8662e9d4e703ece29b51ee1c
phenrici <pascal.henrici@stud.htwk-leipzig.de>
2024-11-11 17:31:52 +0100

    fix: ProgressBar
    TODO: Testing

--- frontend/src/components/Charts/ProgressBar.jsx
+++ frontend/src/components/Charts/ProgressBar.jsx
@@ -1,14 +1,15 @@
+import React from 'react';
 import LinearProgress from '@mui/material/LinearProgress';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
 /**
- * A linear progress component with a label showing the percentage value.
+ * LinearProgressWithLabel component that displays a progress bar with a percentage label.
  * @param {Object} props - Component properties.
  * @param {number} props.value - The progress value as a percentage (0-100).
- * @returns {JSX.Element} A linear progress component with a percentage label.
+ * @returns {JSX.Element} A styled linear progress bar with a label.
  */
-const LinearProgressWithLabel = ({ value }) => {
+function LinearProgressWithLabel({ value }) {
     return (
         <Box sx={{ display: 'flex', alignItems: 'center' }}>
             <Box sx={{ width: '100%', mr: 1 }}>
@@ -19,6 +20,6 @@ const LinearProgressWithLabel = ({ value }) => {
             </Box>
         </Box>
     );
-};
+}
 
 export default LinearProgressWithLabel;
