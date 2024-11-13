eddd05e2b5574af7d31062a36b93f8d772a6ecd8
Robin Kehl <robin.kehl@singular-it.de>
2024-11-13 13:41:19 +0100

    test: unique spy and stub names

--- frontend/src/components/QuestionList/QuestionListItem.cy.jsx
+++ frontend/src/components/QuestionList/QuestionListItem.cy.jsx
@@ -27,10 +27,10 @@ describe('QuestionListItem Component Tests', () => {
         cy.get('[data-cy="question-comment"]').should('exist');
     });
 
-    it('updates question object correctly when a checkbox is selected', () => {
+    it.only('updates question object correctly when a checkbox is selected', () => {
         cy.wait(500); // Füge eine kurze Pause hinzu
 
-        const onChangeSpy = cy.spy().as('onChange');
+        const onChangeSpy = cy.spy().as('onChangeSpy');
         cy.mount(
             <QuestionListItem
                 question={exampleQuestion}
@@ -41,32 +41,32 @@ describe('QuestionListItem Component Tests', () => {
 
         cy.get('input[type="checkbox"]').eq(3).click();
 
-        cy.get('@onChange').should('have.been.calledOnceWith', exampleQuestion.id, { ...exampleQuestion, points: '3', nA: false });
+        cy.get('@onChangeSpy').should('have.been.calledOnceWith', exampleQuestion.id, { ...exampleQuestion, points: '3', nA: false });
 
         cy.get('input[type="checkbox"]').eq(6).click();
-        cy.get('@onChange').should('have.been.calledWith', exampleQuestion.id, { ...exampleQuestion, points: null, nA: true });
+        cy.get('@onChangeSpy').should('have.been.calledWith', exampleQuestion.id, { ...exampleQuestion, points: null, nA: true });
 
         cy.mount(
             <QuestionListItem
                 question={{...exampleQuestion, points: 3, nA: false}}
                 options={options}
-                onChange={cy.stub().as('onChange')}
+                onChange={cy.stub().as('onChangeStub')}
             />
         );
 
         cy.get('input[type="checkbox"]').eq(3).click();
-        cy.get('@onChange').should('have.been.calledWith', exampleQuestion.id, { ...exampleQuestion, points: null, nA: null });
+        cy.get('@onChangeStub').should('have.been.calledWith', exampleQuestion.id, { ...exampleQuestion, points: null, nA: null });
 
         cy.mount(
             <QuestionListItem
                 question={{...exampleQuestion, nA: true}}
                 options={options}
-                onChange={cy.stub().as('onChange')}
+                onChange={cy.stub().as('onChangeStub2')}
             />
         );
 
         cy.get('input[type="checkbox"]').eq(6).click();
-        cy.get('@onChange').should('have.been.calledWith', exampleQuestion.id, { ...exampleQuestion, points: null, nA: null });
+        cy.get('@onChangeStub2').should('have.been.calledWith', exampleQuestion.id, { ...exampleQuestion, points: null, nA: null });
     });
 
     it('updates comment in question object correctly when text is entered in the comment box', () => {
