817d866964c638b2fcdaa6d0428d8aa04f97bd9c
Anna Liepelt <anna.liepelt@stud.htwk-leipzig.de>
2024-11-18 13:22:04 +0100

    feat: sorting questions by category and id with category heading

--- frontend/src/components/QuestionList/CategoryList.jsx
+++ frontend/src/components/QuestionList/CategoryList.jsx
@@ -2,26 +2,26 @@ import {QuestionListItem} from "./QuestionListItem.jsx";
 import {useState} from "react";
 import {CategoryListItem} from "./CategoryListItem.jsx";
 
-export function CategoryList({ questions, options, onChange }) {
+export function CategoryList({ categories, options, onChange }) {
     const labels = [0, 1, 2, 3, 4, 5, "N/A"];
-    const [categories, setCategories] = useState([
-        {
-            name: 'VPN',
-            id: 1,
-            questions: [
-                { id: '1', question: 'Frage 1?', points: 3, nA: false, comment: 'Ok' },
-                { id: '2', question: 'Frage 2?', points: null, nA: true, comment: '' },
-            ]
-        },
-        {
-            name: 'Network',
-            id: 2,
-            questions: [
-                { id: '3', question: 'Frage 3?', points: 2, nA: false, comment: '' },
-                { id: '4', question: 'Frage 4?', points: null, nA: null, comment: 'Schlecht' },
-            ]
-        }
-    ]);
+    // const [categories, setCategories] = useState([
+    //     {
+    //         name: 'VPN',
+    //         id: 1,
+    //         questions: [
+    //             { id: '1', question: 'Frage 1?', points: 3, nA: false, comment: 'Ok' },
+    //             { id: '2', question: 'Frage 2?', points: null, nA: true, comment: '' },
+    //         ]
+    //     },
+    //     {
+    //         name: 'Network',
+    //         id: 2,
+    //         questions: [
+    //             { id: '3', question: 'Frage 3?', points: 2, nA: false, comment: '' },
+    //             { id: '4', question: 'Frage 4?', points: null, nA: null, comment: 'Schlecht' },
+    //         ]
+    //     }
+    // ]);
 
     const handleCategoryChange = (newCategory, updatedQuestion) => {
         const newCategories = categories.map((category) => newCategory.id === category.id? {...category, ...newCategory} : category);
@@ -35,7 +35,7 @@ export function CategoryList({ questions, options, onChange }) {
                 <div key={category.id}>
                     <CategoryListItem
                         category={category}
-                        options={labels}
+                        options={options}
                         onChange={handleCategoryChange}
                     />
                 </div>
--- frontend/src/pages/PerformAudit.jsx
+++ frontend/src/pages/PerformAudit.jsx
@@ -26,6 +26,93 @@ export function PerformAudit() {
 
     const labels = [0, 1, 2, 3, 4, 5, "N/A"];
 
+    const backendData = [
+        {
+            category: { id: 1, name: "VPN", deletedAt: null },
+            id: 76,
+            comment: "hi",
+            points: 3,
+            nA: false,
+            question: "Frage 1?"
+        },
+        {
+            category: { id: 1, name: "VPN", deletedAt: null },
+            id: 44,
+            comment: "",
+            points: null,
+            nA: true,
+            question: "Frage 2?"
+        },
+        {
+            category: { id: 2, name: "Network", deletedAt: null },
+            id: 80,
+            comment: "",
+            points: 2,
+            nA: false,
+            question: "Frage 3?"
+        },
+        {
+            category: { id: 2, name: "Network", deletedAt: null },
+            id: 24,
+            comment: "Schlecht",
+            points: null,
+            nA: null,
+            question: "Frage 4?"
+        },
+        {
+            category: { id: 2, name: "Network", deletedAt: null },
+            id: 11,
+            comment: "Noch schlechter",
+            points: 1,
+            nA: false,
+            question: "Frage 5?"
+        }
+    ];
+
+    const transformData = (questions) => {
+        //sorts questions by category and id
+        const sortedData = questions.sort((a, b) => {
+            if (a.category.id === b.category.id) {
+                return a.id - b.id;
+            }
+            return a.category.id - b.category.id;
+        });
+
+        //sorts questions in a new array of categories with questions
+        const transformedData = sortedData.reduce((acc, item) => {
+            const existingCategory = acc.find(cat => cat.id === item.category.id);
+
+            if (existingCategory) {
+                existingCategory.questions.push({
+                    id: item.id,
+                    question: item.question,
+                    points: item.points,
+                    nA: item.nA,
+                    comment: item.comment,
+                });
+            } else {
+                acc.push({
+                    name: item.category.name,
+                    id: item.category.id,
+                    questions: [
+                        {
+                            id: item.id,
+                            question: item.question,
+                            points: item.points,
+                            nA: item.nA,
+                            comment: item.comment,
+                        }
+                    ]
+                });
+            }
+
+            return acc;
+        }, []);
+
+        console.log("TransformedData: ", transformedData);
+        return transformedData;
+    }
+
     /**
      * Fetches questions from the backend for the current audit on component mount
      * or when the audit ID changes.
@@ -35,7 +122,9 @@ export function PerformAudit() {
         api.get(`/v1/audits/${auditId}/ratings`)
             .then(response => {
                 setQuestions(response.data.sort((a, b) => a.id - b.id));
-                console.log(response.data);
+                console.log("Questions nach get: " ,questions);
+                setSortedQuestions(transformData(backendData));
+                console.log("SortedQuestions nach get: ", sortedQuestions);
             })
             .catch(err => {
                 console.error('Error fetching data:', err);
@@ -86,11 +175,11 @@ export function PerformAudit() {
     return (
         <LayoutDefault>
             <Title>Audit durchführen</Title>
-            <CategoryList
-                // questions={}
-                // options={labels}
-                // onChange={handleQuestionUpdate}
-            />
+            {/*<CategoryList*/}
+            {/*    questions={sortedQuestions}*/}
+            {/*    options={labels}*/}
+            {/*    onChange={handleQuestionUpdate}*/}
+            {/*/>*/}
         </LayoutDefault>
     )
 }
