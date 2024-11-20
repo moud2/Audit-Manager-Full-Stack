02d7b88edb6036f93c01194d9b6b98a33fac6425
Mahamoud <mahamoud_hafed.mohamed@stud.htwk-leipzig.de>
2024-11-20 22:22:04 +0100

    Einen neuen Controller und eine neue Klasse für das PDF aufbauen sowie eine Testklasse erstellen.

--- src/main/java/com/insight/backend/controller/ExportController.java
+++ src/main/java/com/insight/backend/controller/ExportController.java
@@ -1,20 +1,30 @@
 package com.insight.backend.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.insight.backend.model.Rating;
+import com.insight.backend.repository.RatingRepository;
 import com.insight.backend.service.audit.FindAuditService;
 import com.insight.backend.service.rating.FindRatingService;
+import com.insight.backend.service.rating.PdfGenerator;
 import com.insight.backend.service.rating.SaveRatingService;
 import com.insight.backend.mapper.RatingMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 
 @RestController
 public class ExportController {
@@ -26,6 +36,8 @@ public class ExportController {
     private final RatingMapper ratingMapper;
 
     private static final String FILE_DIRECTORY = "/home/kdik96/backend/src/main/resources/static/export_pdf/";
+    private final RatingRepository ratingRepository;
+    private final PdfGenerator pdfGenerator;
 
     /**
      * Constructs a RatingController with the specified services and mapper.
@@ -37,12 +49,14 @@ public class ExportController {
      * @param ratingMapper the mapper to convert Rating to RatingDTO
      */
     @Autowired
-    public ExportController(ObjectMapper objectMapper, FindRatingService findRatingService, SaveRatingService saveRatingService, FindAuditService findAuditService, RatingMapper ratingMapper) {
+    public ExportController(ObjectMapper objectMapper, FindRatingService findRatingService, SaveRatingService saveRatingService, FindAuditService findAuditService, RatingMapper ratingMapper, RatingRepository ratingRepository, PdfGenerator pdfGenerator) {
         this.objectMapper = objectMapper;
         this.findRatingService = findRatingService;
         this.saveRatingService = saveRatingService;
         this.findAuditService = findAuditService;
         this.ratingMapper = ratingMapper;
+        this.ratingRepository = ratingRepository;
+        this.pdfGenerator = pdfGenerator;
     }
 
     /**
@@ -84,4 +98,29 @@ public class ExportController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
+
+
+    @GetMapping(path = "/api/v1/ratings/{auditId}/export", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<InputStreamResource> auditReport(@PathVariable("auditId") long auditId) throws IOException {
+        try {
+
+            // Generate PDF
+            ByteArrayInputStream bis = pdfGenerator.createPdf(auditId);
+
+            // Set HTTP headers
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Disposition", "inline; filename=Report.pdf");
+
+            // Return the PDF as a response
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .contentType(MediaType.APPLICATION_PDF)
+                    .body(new InputStreamResource(bis));
+
+        } catch (Exception e) {
+            // Temporary error handling
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
 }
