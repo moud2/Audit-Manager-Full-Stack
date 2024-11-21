cdd948a451b34027a98d40268d71fe6d5923654c
Matthias Lauer <matthias.lauer@stud.htwk-leipzig.de>
2024-11-21 18:18:01 +0100

    fix: fix imports in AuditsControllerTestHttpDelete

--- src/test/java/com/insight/backend/controller/AuditsControllerTestHttpDelete.java
+++ src/test/java/com/insight/backend/controller/AuditsControllerTestHttpDelete.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
