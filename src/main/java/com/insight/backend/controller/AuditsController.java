5f575ed71aed830e99180c0cee98478364cbef8b
Matthias Lauer <matthias.lauer@stud.htwk-leipzig.de>
2024-11-21 17:48:22 +0100

    fix: change imports in AuditsController according to previous practice

--- src/main/java/com/insight/backend/controller/AuditsController.java
+++ src/main/java/com/insight/backend/controller/AuditsController.java
@@ -15,12 +15,7 @@ import com.insight.backend.service.audit.DeleteAuditService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * AuditsController is a REST controller that handles HTTP requests related to audits.
