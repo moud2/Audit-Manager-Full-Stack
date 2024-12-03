cdbb2c74d6898828c39938b212b857ba5ebcc035
Raad <saif.raadsh1998@gmail.com>
2024-12-03 20:56:37 +0100

    Header is added

--- frontend/src/layouts/LayoutDefault.jsx
+++ frontend/src/layouts/LayoutDefault.jsx
@@ -1,15 +1,19 @@
-import {DashboardSidebar} from "../components/Layout/DashboardSidebar.jsx";
-import {Children} from "react";
+import { DashboardSidebar } from "../components/Layout/DashboardSidebar.jsx";
+import { Children } from "react";
+import { Header } from "../components/Layout/Header.jsx";
 
-export function LayoutDefault({children}){
-    return (
-        <div className="grid grid-cols-12 h-screen w-screen">
-            <aside className="col-span-2">
-                <DashboardSidebar></DashboardSidebar>
-            </aside>
-            <main className="col-span-10 h-full">
-                {Children.map(children, child => child)}
-            </main>
-        </div>
-    )
+export function LayoutDefault({ children }) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex flex-1 mt-16">
+        <aside className="w-64 h-[calc(100vh-4rem)] bg-gray-200 p-4 shadow-md fixed">
+          <DashboardSidebar></DashboardSidebar>
+        </aside>
+        <main className="flex-1 ml-64 p-4 bg-gray-100">
+          {Children.map(children, (child) => child)}
+        </main>
+      </div>
+    </div>
+  );
 }
