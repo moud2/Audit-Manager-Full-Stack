4c862cb8e99964382ca9e7514ac0d0068ace59c0
Matthes Klemt <matthes.klemt@stud.htwk-leipzig.de>
2024-12-09 17:51:48 +0100

    fix-287: edited comment

--- src/main/java/com/insight/backend/exception/AuditDeletedException.java
+++ src/main/java/com/insight/backend/exception/AuditDeletedException.java
@@ -2,7 +2,7 @@ package com.insight.backend.exception;
 
 /**
  * Exception thrown when a requested audit is marked as deleted.
- * This exception results in an HTTP status code of 410 (GONE).
+ * This exception results in an HTTP status code of 404 (Not Found).
  */
 public class AuditDeletedException extends RuntimeException {
     public AuditDeletedException(Long auditId) {
