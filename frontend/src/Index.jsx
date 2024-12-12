2fd01dd2b06cdcc7aa17ac2922ea2600bdda4ace
Merlin Schuckert <merlin.schuckert@stud.htwk-leipzig.de>
2024-12-12 13:48:34 +0100

    feat: Sidebar implemented, final update

--- frontend/src/Index.jsx
+++ frontend/src/Index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { SidebarProvider } from "./layouts/SidebarContext";
 import './index.css'
+import { SidebarProvider } from './layouts/SidebarContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
