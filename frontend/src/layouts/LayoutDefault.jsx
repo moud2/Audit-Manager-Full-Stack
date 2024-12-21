83a69811b3fae29baced97de170514ab7e152aa7
Anna Liepelt <anna.liepelt@stud.htwk-leipzig.de>
2024-12-21 13:26:54 +0100

    fix: changed sidebar style,  tailwind classes, indentation style

--- frontend/src/layouts/LayoutDefault.jsx
+++ frontend/src/layouts/LayoutDefault.jsx
@@ -13,90 +13,103 @@ import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
-import {Children} from "react";
 import { Link } from "react-router-dom";
 import { useSidebar } from "./UseSidebar.jsx";
+import { Footer } from "../components/Layout/Footer.jsx";
 
 const drawerWidth = 240;
 
 const Main = styled('main', {
     shouldForwardProp: (prop) => prop !== 'open',
-  })(({ theme, open }) => ({
+})(({ theme, open }) => ({
     flexGrow: 1,
     padding: theme.spacing(3),
     marginLeft: open ? 0 : `-${drawerWidth}px`,
     transition: theme.transitions.create('margin', {
-      easing: open
-        ? theme.transitions.easing.easeOut
-        : theme.transitions.easing.sharp,
-      duration: open
-        ? theme.transitions.duration.enteringScreen
-        : theme.transitions.duration.leavingScreen,
+        easing: open
+            ? theme.transitions.easing.easeOut
+            : theme.transitions.easing.sharp,
+        duration: open
+            ? theme.transitions.duration.enteringScreen
+            : theme.transitions.duration.leavingScreen,
     }),
-  }));
+}));
 
-const DrawerHeader = styled('div')(({ theme }) => 
-  ({display: 'flex',
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
     alignItems: 'center',
     padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,justifyContent: 'flex-end',}));
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+}));
 
-export function LayoutDefault({children}) {
-  const theme = useTheme();
-  const { open, closeSidebar, openSidebar } = useSidebar();
-  
-  const links = 
-  [{ href: "/dashboard", label: "Dashboard" },];
+export function LayoutDefault({ children }) {
+    const theme = useTheme();
+    const { open, closeSidebar, openSidebar } = useSidebar();
 
-  return (
-    <Box sx={{display: 'flex'}}>
-      <CssBaseline/>
-      <Header/>
-      <IconButton 
-        color="inherit" 
-        aria-label="open drawer" 
-        onClick={openSidebar} 
-        data-cy="menu-icon"
-        edge="start" 
-        sx={{position: "fixed",top: "16px",left: "215px",zIndex: 1301,display: open ? "none" : "block",}}>
-        <MenuIcon/>
-      </IconButton>
-     <Drawer
-        data-cy="sidebar" 
-        sx={{width: drawerWidth, flexShrink: 0,'& .MuiDrawer-paper': {width: drawerWidth,boxSizing: 'border-box',},}} 
-        variant="persistent" 
-        anchor="left" 
-        open={open}>
+    const links = [{ href: "/dashboard", label: "Dashboard" }];
 
-        <DrawerHeader>
-        <div style={{ display: "flex", alignItems: "center" }}>
-            <img
-              src="/logo-insight.png"
-              alt="Logo"
-              style={{ width: "32px", height: "32px", marginRight: "8px" }}
-            />
-            <h2 style={{ margin: 0, fontSize: "1.2rem" }}>InSight</h2>
-          </div>
-
-          <IconButton onClick={closeSidebar} data-cy="close-icon">
-            {theme.direction === 'ltr' ? <ChevronLeftIcon/> : <ChevronRightIcon/>}
-          </IconButton>
-        </DrawerHeader>
-        <Divider/>
-        <List>
-          {links.map((link, index) => (
-          <ListItem key={index} disablePadding>
-          <ListItemButton component={Link} to={link.href}>
-          <ListItemText primary={link.label}/>
-          </ListItemButton>
-          </ListItem>))}
-        </List>
-        <Divider/>
-      </Drawer>
-      <Main open={open} className="flex-1 ml-64 bg-gray-100 pb-24">
-        <DrawerHeader/>
-        {Children.map(children, (child) => child)}
-      </Main>
-    </Box>
-  );
+    return (
+        <Box className="flex">
+            <CssBaseline />
+            <Header />
+            <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={openSidebar}
+                data-cy="menu-icon"
+                edge="start"
+                sx={{
+                    position: "fixed",
+                    top: "9px",
+                    left: "215px",
+                    zIndex: 1301,
+                    display: open ? "none" : "block",
+                }}
+            >
+                <MenuIcon />
+            </IconButton>
+            <Drawer
+                data-cy="sidebar"
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': { width: drawerWidth, boxSizing: 'border-box' },
+                }}
+                variant="persistent"
+                anchor="left"
+                open={open}
+            >
+                <DrawerHeader>
+                    <div className="flex items-center mt-2">
+                        <img
+                            src="/logo-insight.png"
+                            alt="Logo"
+                            className="w-8 h-8 mr-2"
+                        />
+                        <h2 className="text-4xl font-medium">InSight</h2>
+                    </div>
+                    <IconButton onClick={closeSidebar} data-cy="close-icon">
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                    </IconButton>
+                </DrawerHeader>
+                <Divider className="pt-2" />
+                <List>
+                    {links.map((link, index) => (
+                        <ListItem key={index} disablePadding>
+                            <ListItemButton component={Link} to={link.href}>
+                                <ListItemText primary={link.label} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+                <Divider />
+            </Drawer>
+            <Main open={open} className="flex-1 ml-64 bg-gray-100 pb-24">
+                <DrawerHeader />
+                {children}
+            </Main>
+            <Footer />
+        </Box>
+    );
 }
