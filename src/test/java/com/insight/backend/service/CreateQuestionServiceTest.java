f933feac0c49001285071ddcc830e2f33988dcf6
Mahamoud <mahamoud_hafed.mohamed@stud.htwk-leipzig.de>
2025-01-12 13:06:29 +0100

    fix: corrected the order of import statements

--- src/test/java/com/insight/backend/service/CreateQuestionServiceTest.java
+++ src/test/java/com/insight/backend/service/CreateQuestionServiceTest.java
@@ -1,20 +1,22 @@
 package com.insight.backend.service;
 
-import java.util.List;
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.insight.backend.dto.NewQuestionDTO;
