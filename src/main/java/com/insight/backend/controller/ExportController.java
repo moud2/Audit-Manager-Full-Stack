2505eda6bb2d6b0bc19b6296ab67cf7009d361e8
Thomas Mugler <thomas.mugler@stud.htwk-leipzig.de>
2025-01-12 19:32:30 +0100

    feat: Provide Functions from different Branch for .csv-Export development
    Provide Functions from different Branch for .csv-Export development while branch #249 is still in progress to accelarte .csv-Export realization

---
+++ src/main/java/com/insight/backend/controller/ExportController.java
@@ -0,0 +1,58 @@
+package com.insight.backend.controller;
+
+import com.insight.backend.service.rating.PdfGeneratorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+@RestController
+public class ExportController {
+    private final PdfGeneratorService pdfGenerator;
+
+    /**
+     * Constructs a ExportController with the specified services and mapper.
+     * @param pdfGenerator the mapper to convert ratings to pdf
+     */
+    @Autowired
+    public ExportController(PdfGeneratorService pdfGenerator) {
+        this.pdfGenerator = pdfGenerator;
+    }
+
+    /**
+     * Downloads a file from the export_pdf directory
+     *
+     * @param auditId the name of the audit to download
+     * @return the file as a response
+     */
+
+    @GetMapping(path = "/api/v1/audits/{auditId}/export", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<InputStreamResource> auditReport(@PathVariable("auditId") long auditId) throws IOException {
+        try {
+
+            // Generate PDF
+            ByteArrayInputStream bis = pdfGenerator.createPdf(auditId);
+
+            // Set HTTP headers
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Disposition", "inline; filename=Report.pdf");
+
+            // Return the PDF as a response
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .contentType(MediaType.APPLICATION_PDF)
+                    .body(new InputStreamResource(bis));
+
+        } catch (Exception e) {
+            // Temporary error handling
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
