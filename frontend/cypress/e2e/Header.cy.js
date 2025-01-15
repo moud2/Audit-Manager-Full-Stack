b39689bb471a9e6ad382d77ce279949982c04b1c
Abolfazl <abolfazl.shahbazfar@stud.htwk-leipzig.de>
2025-01-15 18:20:53 +0100

    Header test fixed

--- frontend/cypress/e2e/Header.cy.js
+++ frontend/cypress/e2e/Header.cy.js
@@ -1,20 +1,35 @@
-describe('<Header />', () => {
-  it("Visits InSight (Localhost)", () => {
+describe("<Header />", () => {
+  beforeEach(() => {
+    // Besuche die Startseite vor jedem Test
     cy.visit("http://localhost:5173/#");
+  });
+
+  it("Visits InSight (Localhost)", () => {
+    // Überprüfen, ob die URL korrekt geladen wurde
     cy.url().should("include", "localhost:5173");
   });
 
-  it('Looks for the heading "InSight"', () => {
-    cy.visit("http://localhost:5173/#");
-    cy.contains("InSight").should("be.visible");
+  it('Checks for the heading "InSight"', () => {
+    // Überprüfen, ob die Überschrift "InSight" sichtbar ist
+    cy.contains("h1", "InSight").should("be.visible");
   });
 
   it("Renders the logo image", () => {
-    cy.visit("http://localhost:5173/#");
+    // Überprüfen, ob das Logo korrekt angezeigt wird
     cy.get('img[alt="InSight Logo"]')
-      .should("be.visible")
-      .and("have.attr", "src")
-      .and("include", "logo-insight.png");
+      .should("be.visible") // Sichtbar sein
+      .and("have.attr", "src") // Attribut 'src' haben
+      .and("include", "logo-insight.png"); // 'src' enthält den Bildnamen
+
+    // Überprüfen, ob das alt-Attribut korrekt gesetzt ist
     cy.get('img[alt="InSight Logo"]').should("have.attr", "alt", "InSight Logo");
+  });
+
+  it("Has correct header styles", () => {
+    // Überprüfen, ob der Header korrekt positioniert ist
+    cy.get("header")
+      .should("have.class", "fixed")
+      .and("have.css", "top", "0px")
+      .and("have.css", "background-color", "rgb(255, 255, 255)"); // Weißer Hintergrund
   });
 });
