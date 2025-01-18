e922186ea69f9f3e00298938a3613a5fd707e736
Abolfazl <abolfazl.shahbazfar@stud.htwk-leipzig.de>
2025-01-18 14:51:07 +0100

    pipeline und performaudit test ixed

--- frontend/cypress/e2e/PerformAudit.cy.jsx
+++ frontend/cypress/e2e/PerformAudit.cy.jsx
@@ -56,7 +56,7 @@ describe('PerformAudit Page', () => {
         });
     });
 
-    it('should handle checkbox interactions correctly', () => {
+    it.only('should handle checkbox interactions correctly', () => {
         cy.get('input[type="checkbox"]').eq(0).check({ force: true }).should('be.checked');
         cy.get('input[type="checkbox"]').eq(0).uncheck({ force: true }).should('not.be.checked');
         cy.get('input[type="checkbox"]').eq(1).check({ force: true }).should('be.checked');
