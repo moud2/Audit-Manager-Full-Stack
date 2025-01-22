c2c810de6db1264221d3c11d2b22a403bfe58259
Anna Liepelt <anna.liepelt@stud.htwk-leipzig.de>
2025-01-22 18:17:13 +0100

    feat: adds a first version for csv uploads

--- frontend/src/pages/ManageCategoriesAndQuestions.jsx
+++ frontend/src/pages/ManageCategoriesAndQuestions.jsx
@@ -1,15 +1,43 @@
 import { LayoutDefault } from "../layouts/LayoutDefault.jsx";
 import { Button } from "@mui/material";
 import Title from "../components/Textareas/Title.jsx";
+import {useState} from "react";
 
 export function ManageCategoriesAndQuestions() {
     const handleExportQuestionsClick = () => {
 
     }
 
-    const handleImportQuestionsClick = () => {
+    const [file, setFile] = useState(null);
+    const [uploadStatus, setUploadStatus] = useState('');
 
+
+    const handleFileAuswahl = (e) => {
+        console.log(e.target.files[0]);
+        setFile(e.target.files[0]);
     }
+
+    const handleFileUpload = async () => {
+        const formData = new FormData();
+        formData.append('file', file);
+
+        try {
+            const response = await fetch('/api/database/upload', {
+                method: 'POST',
+                body: formData,
+            });
+
+            if (response.ok) {
+                setUploadStatus('Upload erfolgreich!');
+            } else {
+                setUploadStatus('Upload fehlgeschlagen.');
+            }
+        } catch (error) {
+            console.error('Fehler beim Hochladen:', error);
+            setUploadStatus('Ein Fehler ist aufgetreten.');
+        }
+    };
+
 
     return (
         <LayoutDefault>
@@ -25,12 +53,15 @@ export function ManageCategoriesAndQuestions() {
                 </Button>
                 <Button
                     data-cy="ImportQuestionsButton"
-                    onClick={handleImportQuestionsClick}
+                    // onClick={handleImportQuestionsClick}
                     variant="outlined"
                     color="error"
                 >
                     Daten importieren
                 </Button>
+                <input type="file" accept=".csv" onChange={e => handleFileAuswahl(e)}/>
+                <button onClick={handleFileUpload}>Hochladen</button>
+                {uploadStatus && <p>{uploadStatus}</p>}
             </div>
         </LayoutDefault>
     )
