012fec04604eb0dd900dc93f211feab7b252fde5
Anna Liepelt <anna.liepelt@stud.htwk-leipzig.de>
2025-01-26 15:14:42 +0100

    fix: deletes unused imports and routings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,6 @@ import {Dashboard} from "./pages/Dashboard.jsx";
 import {NewAudit} from "./pages/NewAudit.jsx";
 import {PerformAudit} from "./pages/PerformAudit.jsx";
 import {Evaluation} from "./pages/Evaluation.jsx";
-import {NewCategory} from "./pages/NewCategory.jsx";
-import {NewQuestion} from "./pages/NewQuestion.jsx";
 import { CompareAudits} from './pages/CompareAudits.jsx';
 import {ManageCategoriesAndQuestions} from "./pages/ManageCategoriesAndQuestions.jsx";
 
@@ -18,9 +16,7 @@ function App() {
                 <Route path="/dashboard" element={<Dashboard />}></Route>
                 <Route path="/new-audit" element={<NewAudit />}></Route>
                 <Route path="/perform-audit/:auditId" element={<PerformAudit />}></Route>
-                <Route path="/new-category" element={<NewCategory />}></Route>
                 <Route path="/evaluation/:auditId" element={<Evaluation />} />
-                <Route path="/new-question" element={<NewQuestion />}></Route>
                 <Route path="/compare-audits/:auditId" element={<CompareAudits />} />
                 <Route path="/manage-categories-and-questions" element={<ManageCategoriesAndQuestions />} />
             </Routes>
